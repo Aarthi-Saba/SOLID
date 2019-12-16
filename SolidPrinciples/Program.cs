0f1eb2914b158a9f5cbc81058b4b13157f3414f5
Abishek RS <aarthi.saba@gmail.com>
2019-12-15 19:16:06 -0500

    Add project files.

---
+++ SolidPrinciples/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace SolidPrinciples
+{
+    public class Program
+    {
+       // private static Computer _computer;
+        public static void Main()
+        {
+            IComputer comp = new Computer();
+            TestComputer.TestProcess(comp);
+        }
+    }
+
+    public class TestComputer
+    {
+        public static void TestProcess(IComputer comp)
+        {
+            Console.WriteLine($"Power supply : {comp.ProducePower()}");
+            Console.WriteLine($"Cpu process : {comp.Calculate()}");
+            Console.WriteLine($"RAM : {comp.StoringData()}");
+            Console.WriteLine($"HardDrive : {comp.WritingData()}");
+            Console.WriteLine($"Motherboard : {comp.Motherboarding()}");
+        }
+    }
+    public interface IComputer : IPower,IHarddrive,IRam,ICpu,IMotherboard
+    {
+    }
+
+    public interface IMotherboard
+    {
+        public string Motherboarding();
+    }
+
+    public interface ICpu
+    {
+        public string Calculate();
+    }
+
+    public interface IHarddrive
+    {
+        public string WritingData();
+    }
+
+    public interface IRam
+    {
+        public string StoringData();
+    }
+
+    public interface IPower
+    {
+        public string ProducePower();
+    }
+
+    public class Computer : IComputer
+    {
+        public string Calculate()
+        {
+            return "Calculating";
+        }
+
+        public string Motherboarding()
+        {
+            return "Connecting everything";
+        }
+
+        public string ProducePower()
+        {
+            return "Producing Power";
+        }
+
+        public string StoringData()
+        {
+            return "Storing Data";
+        }
+
+        public string WritingData()
+        {
+            return "Writing Data";
+        }
+    }
+}
